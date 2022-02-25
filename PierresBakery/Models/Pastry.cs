9887ad0960a3a00c4bb7b8088847df92138c3975
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-25 11:49:08 -0800

    Add code to pass --price of three  pastry

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -15,8 +15,27 @@ namespace PierresBakery.Models
     public int CalculatePastryPrice(int inputQuantity)
     {
       int price = 2;
+      int priceOfThree = 5;
       int totalPrice; 
+      int threeRemainder = inputQuantity % 3;
+      int countOfThree = inputQuantity / 3;
+      if(inputQuantity<3)
+      {
       totalPrice = price * inputQuantity;
+      }
+      else
+      {
+        if(threeRemainder == 0)
+        {
+          totalPrice = countOfThree * priceOfThree;
+        }
+        else
+        {
+          totalPrice = countOfThree * priceOfThree;
+          totalPrice += threeRemainder * price;
+        }
+
+      }
       return totalPrice;
     }
 
